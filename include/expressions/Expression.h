16aea678a47abc8e85a46d87591c2ded3bbd3952
Alice <alisarit664@gmail.com>
2024-04-18 23:20:30 +0300

    fix warning

--- include/expressions/Expression.h
+++ include/expressions/Expression.h
@@ -5,4 +5,4 @@ typedef struct _Expression Expression;
 Expression* Expression_create(const char* expression_string);
 void Expression_destroy(Expression* e);
 
-float Expression_evaluate(const Expression* e, float x, float y);
+float Expression_evaluate(const void* data, float x, float y);
