27f675ac3830c3ecea8b5df4773d13264ebc7701
Alice <alisarit664@gmail.com>
2024-05-03 22:13:59 +0300

    add grid

--- include/render/Render.h
+++ include/render/Render.h
@@ -3,5 +3,8 @@
 #include "Primitives.h"
 #include "PlotData.h"
 
+void RenderGrid(Region plotRegion, Region renderRegion,
+                void (*renderFunc)(void*, Point, Point), void* data);
+
 void RenderPlot(const PlotData* plotData, Region renderRegion,
                 void (*renderFunc)(void*, Point, Point), void* data);
